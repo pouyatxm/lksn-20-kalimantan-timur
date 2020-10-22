4ea2af444a9e8d3ab98120828e74cfc2eda6e343
Clash <clashofc3124@gmail.com>
2020-10-22 10:41:41 +0800

    4 commit

--- game.js
+++ game.js
@@ -1,7 +1,3 @@
 let button = document.getElementById("restart");
 
-    if(button.onclick = true) {
-        button.classList.add('bActive');
-    } else {
-        button.classList.remove('bActive')
-    }
+
