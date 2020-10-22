deec321b61f7de156600c9701449f2797e034aaa
Clash <clashofc3124@gmail.com>
2020-10-22 14:15:00 +0800

    commit

--- PYTON/phyton.js
+++ PYTON/phyton.js
@@ -1,14 +1,19 @@
 export let PySpeed = 1
 import { GBoard } from './system.js'
 const PhytonBody = [
-    { x: 15, y: 24 }
+    { x: 15, y: 24 },
+    { x: 16, y: 24 },
+    { x: 17, y: 24 },
+    { x: 18, y: 24 },
+    { x: 19, y: 24 },
+    { x: 20, y: 24 }
 ]
 
 export function Update () {
     
 }
 
-export function Render (board) {
+export function Render (GBoard) {
     PhytonBody.forEach(Sg => {
         var PhytonElement = document.createElement('div')
         PhytonElement.style.gridRowStart = Sg.y
