5cea74c22e2c7e26e9004e3506dce05aaa3467c2
Clash <clashofc3124@gmail.com>
2020-10-22 11:28:00 +0800

    commit

--- system.js
+++ system.js
@@ -1,7 +1,15 @@
 let ResButton = document.getElementById("restart");
 let start = document.getElementById("start");
 let Lrt = 0;
+let PySpeed = 1000; // 1000=1s
+var global = {};
 
-setInterval( Function => {
-    console.log('1s Every time!')
-}, 1000);
+function GameStart() {
+    var GS = setInterval( Function => {
+        console.log('1s Every time!')
+    }, `${PySpeed}`);
+} 
+
+let PyPos = [
+    { x:10, y:10 }
+]
